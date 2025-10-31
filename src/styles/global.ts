006cf5285b659db2bb0d3f0c9685e159b7e41e57
afernandez <alejandro.fernandez@ice.com>
2025-10-31 15:11:55 -0300

    feat: deleting vite seed to base project

---
+++ src/styles/global.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+    width: 100%;
+    overflow: hidden; /* evita scroll fantasma */
+  }
+
+  body {
+    font-family: system-ui, sans-serif;
+    background: linear-gradient(135deg, #ffffff, #f1f3f5);
+  }
+`
