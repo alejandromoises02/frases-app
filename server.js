66bcdddaf870e04e50545ebfa091315920d3dcce
afernandez <alejandro.fernandez@ice.com>
2025-11-02 16:23:33 -0300

    feat: deploying on live

--- server.js
+++ server.js
@@ -39,4 +39,5 @@ app.delete('/phrases/:id', (req, res) => {
   res.status(204).send();
 });
 
-app.listen(4000, () => console.log('Server running on http://localhost:4000'));
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
